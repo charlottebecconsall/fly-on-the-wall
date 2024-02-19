dfc0657577e55a609f5662dfd253aee48a000a7b
Charlotte Becconsall <charlottebecconsall@gmail.com>
2024-02-19 19:06:26 +1300

    Ricoshooter placing

--- main_game.gd
+++ main_game.gd
@@ -1,18 +1,35 @@
 extends Node2D
 
 var num_flies = 10
+var num_ricoshooters = 3
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	# Puts defined number of flies in random places
+func load_flies(num_flies):
 	for i in num_flies:
 		var fly_scene = preload("res://fly.tscn")
 		var fly = fly_scene.instantiate() as Node2D
 		add_child(fly)
-		fly.position = Vector2(randi() % get_window().size.x, randi() % get_window().size.y)
+		fly.position = Vector2(randi() % (get_window().size.x-100), randi() % (get_window().size.y-100))
 		fly.rotation_degrees = randi() % 360
+		
 
+func load_ricoshooter_info(num_ricoshooters):
+	$RicoshooterNum.text = str(num_ricoshooters)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Prepares game by loading in all the essentials
+	load_flies(num_flies)
+	load_ricoshooter_info(num_ricoshooters)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if MOUSE_BUTTON_LEFT:
+		if num_ricoshooters > 0:
+			var ricoshooter_scene = preload("res://ricoshooters.tscn")
+			var ricoshooter = ricoshooter_scene.instantiate() as Node2D
+			add_child(ricoshooter)
+			ricoshooter.position = get_global_mouse_position()
+			num_ricoshooters -= 1
+			load_ricoshooter_info(num_ricoshooters)
